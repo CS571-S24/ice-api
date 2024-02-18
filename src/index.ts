df767862c028aa482f15413e139e12646935d2dd
Cole Nelson <ctnelson1997@gmail.com>
2024-02-18 10:58:42 -0600

    updt

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { CS571Initializer } from '@cs571/s24-api-framework'
 import { CS571ChiliRoute } from './routes/chili-route';
 import { CS571PizzaRoute } from './routes/pizza-route';
 import { CS571PastaRoute } from './routes/pasta-route';
+import { CS571HurricanesRoute } from './routes/hurricanes-route';
 
 
 console.log("Welcome to ICE API!");
@@ -21,12 +22,13 @@ const appBundle = CS571Initializer.init(app, {
 const chili = JSON.parse(fs.readFileSync('includes/chili.json').toString())
 const pasta = JSON.parse(fs.readFileSync('includes/pasta.json').toString())
 const pizza = JSON.parse(fs.readFileSync('includes/pizza.json').toString())
-
+const hurr = JSON.parse(fs.readFileSync('includes/hurr.json').toString())
 
 appBundle.router.addRoutes([
   new CS571ChiliRoute(chili),
   new CS571PastaRoute(pasta),
   new CS571PizzaRoute(pizza),
+  new CS571HurricanesRoute(hurr),
 ])
 
 app.listen(appBundle.config.PORT, () => {
